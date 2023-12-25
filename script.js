327f39fd34084e0fbf21fe6bdad8da8db90d136b
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-25 15:18:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	let ans = "";
+
+	for(let ch of s){
+		if(ch === ' ') return ans;
+		ans += ch;
+	}
+
+	return ans;
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
